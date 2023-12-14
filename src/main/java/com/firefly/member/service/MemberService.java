b8833439325faa7da7ad3be87a416ac3413997cd
devhyeon0 <devhyeon0@gmail.com>
2023-12-14 22:42:53 +0900

    feat: Member Service Class 생성

---
+++ src/main/java/com/firefly/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.firefly.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MemberService {
+}
