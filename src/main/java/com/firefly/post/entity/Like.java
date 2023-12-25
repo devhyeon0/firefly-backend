1c0d6c1a94fb7e781304c5e1ae4159f008e7c282
devhyeon0 <devhyeon0@gmail.com>
2023-12-25 18:57:14 +0900

    refactor: 오타 수정

--- src/main/java/com/firefly/post/entity/Like.java
+++ src/main/java/com/firefly/post/entity/Like.java
@@ -15,7 +15,7 @@ public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
+    @Column(name = "like_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
